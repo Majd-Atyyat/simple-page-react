fc808f7fd667431f3a3acb11de6da3b40b47f064
majd atyyat <majd.atyyat@pwc.com>
2023-04-06 03:24:42 +0300

    added form to handling user input

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-import { BrowserRouter } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './maniContent.css'
 
 function MainContent() {
+  const [name, setName] = useState('');
+  const [age, setEmail] = useState('');
+  
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  }
+  
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+  
   return (
     <main>
       <h2>Welcome to my website!</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <form>
+        <label className="purple" htmlFor="name">Write your name:</label>
+        <input  type="text" id="name" value={name} onChange={handleNameChange} />
+        <br />
+        <label className="orange"htmlFor="Age">Write your age:</label>
+        <input type="text" id="age" value={age} onChange={handleEmailChange} />
+      </form>
+      <p>
+        < spin className="purple">{name && `Your name is "${name}" and you're awesome   `}</spin>
+      
+        
+        <br />
+        <spin className="orange">{age && `Your Age  is "${age}", But you're old! :O `}</spin>
+        
+      </p>
     </main>
   );
 }
