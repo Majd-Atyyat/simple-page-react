063ba2defe40b75a9161938a49a847a785888887
majd atyyat <majd.atyyat@pwc.com>
2023-04-09 18:50:22 +0300

    added conditional rendering

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,20 @@ import React, { useState } from 'react';
 
 function Footer() {
   const [backgroundColor, setBackgroundColor] = useState('#810CA8');
+  const [showMessage, setShowMessage] = useState(false);
   
   const handleBackgroundColorChange = () => {
     setBackgroundColor('#EF9A53');
+    setShowMessage(true);
   }
   
   return (
     <footer style={{ backgroundColor }}>
       <p>Copy rights @majd atyyat</p>
       <button onClick={handleBackgroundColorChange}>Change My color</button>
+      {showMessage && backgroundColor === '#EF9A53' && (
+        <p style={{ color: 'white' }}>You changed the color to orange!</p>
+      )}
     </footer>
   );
 }
